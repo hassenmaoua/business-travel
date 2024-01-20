a63983366980826f4c07203367926b22955bb26f
mohamedaminenouri <mohamedamine.nouri@esprit.tn>
2024-01-20 10:22:31 +0100

    add interface

--- src/main/java/edu/businesstravel/HelloApplication.java
+++ src/main/java/edu/businesstravel/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("add_event.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -21,3 +21,5 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
+
---
+++ src/main/java/edu/businesstravel/dao/controller/EventController.java
@@ -0,0 +1,21 @@
+package edu.businesstravel.dao.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EventController implements Initializable {
+    @FXML
+    private Scene scene;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
